703723a96e5cd74b8621273e1d69dd1c22fc9344
Alvin Delagon <aadelagon@voyagerinnovation.com>
2018-09-26 10:39:54 +0800

    Add install script

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Install requirements
+pip install -r requirements.txt
+
+# Install jupyter extensions
+jupyter contrib nbextension install --user
+jupyter nbextensions_configurator enable --user
+jupyter nbextension enable toc2/main
+jupyter nbextension enable code_prettify/code_prettify
+jupyter nbextension enable codefolding/main
+jupyter nbextension enable python-markdown/main
+jupyter nbextension enable exercise2/main
+jupyter nbextension enable codefolding/main
+jupyter nbextension list
+
+
